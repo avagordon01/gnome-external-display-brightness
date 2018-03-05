4af3cfeac3fb29b8d4685186a1e6de7e49852588
Patrick Gordon <patrick@hadean.com>
2018-03-05 23:23:21 +0000

    basic multi monitor support

--- extension.js
+++ extension.js
@@ -1,13 +1,15 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
 
-let overlay;
+let overlays;
 let aggregateMenu;
 let sliderActor;
 let connectId;
 
 function _update() {
-    overlay.opacity = 256 * (1 - sliderActor.value);
+    overlays.forEach(function(overlay) {
+        overlay.opacity = 256 * (1 - sliderActor.value);
+    });
 }
 
 function enable() {
@@ -15,22 +17,31 @@ function enable() {
     sliderActor = aggregateMenu._brightness._slider;
     connectId = sliderActor.connect('value-changed', _update)
 
-    overlay = new St.Label({
-        style_class: 'overlay',
+    overlays = [];
+    Main.layoutManager.monitors.forEach(function(monitor) {
+        let overlay = new St.Label({
+            style_class: 'overlay',
+        });
+        Main.uiGroup.add_actor(overlay);
+
+        overlay.set_width(monitor.width);
+        overlay.set_height(monitor.height);
+        overlay.set_x(monitor.x);
+        overlay.set_y(monitor.y);
+
+        overlays.push(overlay)
     });
-    Main.uiGroup.add_actor(overlay);
 
-    let monitor = Main.layoutManager.primaryMonitor;
-    overlay.set_width(monitor.width);
-    overlay.set_height(monitor.height);
-
-    //overlay.opacity = 256 * (sliderActor.value);
+    overlay.opacity = 256 * (sliderActor.value);
     _update();
 }
 
 function disable() {
     if (connectId)
         sliderActor.disconnect(connectId)
-    Main.uiGroup.remove_actor(overlay);
-    overlay = null;
+    overlays.forEach(function(overlay) {
+        Main.uiGroup.remove_actor(overlay);
+        overlay = null;
+    });
+    overlays = [];
 }
